50d841f3d08cc440f3163782fd65aaee13e0ad1c
Cyngyz <92852651+NazgulM@users.noreply.github.com>
2022-01-27 20:46:49 +0100

    new lessons

---
+++ Learn 2701/src/com/tutorial/RectangleComputation.java
@@ -0,0 +1,36 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class RectangleComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Cylinder Computation
+		//double surface area,  base area, volume of a cylinder;
+		
+		double radius  = 1.2;
+		double h = 3.2;
+		double PI = 3.14;
+		double surfaceArea = 2 * PI * radius*(radius+h);
+		double volume  = PI * radius * radius * h;
+		double baseOfArea = (PI * radius*radius);
+		
+		System.out.println("The surface area of Cylinder: " + surfaceArea);
+		System.out.println("The volume of a cylinder: " + volume);
+		System.out.println("The base of Area: " + baseOfArea);
+		
+		/*double width, height, area, perimeter;
+		Scanner sc = new Scanner(System.in);
+				
+		double width1 = 4.95d;
+		double height1 = 7.95d;
+		double area1 = width1*height1;
+	    double perimeter1 = 2 * (width1 + height1);
+	    System.out.println(area1);
+	    System.out.println(perimeter1);
+		*/
+	}
+
+}
---
+++ Learn 2701/src/com/tutorial/start.java
@@ -0,0 +1,44 @@
+package com.tutorial;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		//Six nad Seven IntegerSum
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+		int num6 = 66;
+		int num7 = 77;
+		int sum = num1 + num2 +num3 + num4 + num5 + num6+ num7;
+		int total = (num1 + num2 + num3 + num4 + num5 + num6)* num7;
+		System.out.println("The sum is: ");
+		System.out.println(sum);
+		System.out.println(total);
+		
+		//PrintSquarePattern
+		/*System.out.println("*****");
+		   System.out.println("*   *");
+		   System.out.println("*   *");
+		   System.out.println("*   *");
+		   System.out.println("*****");*/
+		
+		/*System.out.println("*****");
+	System.out.println(" *****");
+	System.out.println("*****");
+	System.out.println(" *****");
+	System.out.println("*****");*/
+	}
+
+		
+	
+		
+{
+   
+}
+}
