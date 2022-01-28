70bd18b0965de7e0a9004dbd1d903384e65ee40b
Cyngyz <92852651+NazgulM@users.noreply.github.com>
2022-01-28 10:05:20 +0100

    2nd try

--- Learn2801/src/com/tutorial/methodles.java
+++ Learn2801/src/com/tutorial/methodles.java
@@ -19,12 +19,19 @@ public class methodles {
 	}
 	
 	
+    static void adding(int num1, int num2) {
+    	int sum = num1 + num2;
+    	System.out.println(sum);
+    	
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         HelloWorld();
         Message ();
        ChangeArrayValue (1,66); //update array item value
        System.out.println(nums[1]);
+       adding (99, 181);
 	}
 
 }
