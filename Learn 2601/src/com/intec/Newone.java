512cdad7210b54ffc9c5bded2faf985d945bbe43
Cyngyz <92852651+NazgulM@users.noreply.github.com>
2022-01-30 00:15:19 +0100

    add

---
+++ Learn 2601/src/com/intec/Newone.java
@@ -0,0 +1,41 @@
+package com.intec;
+
+public class Newone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*System.out.println("Fizz");
+		for(int i=0;i<a.length;i++){  
+			if(a[i]%2!=0){  
+			System.out.println(a[i] + "fizz");  
+			
+			/*System.out.println("Buzz:");  
+			for(int y=0;i<fizzgame.length;y++){  
+			if(fizzgame[y]%2==0){  
+			System.out.println(fizzgame[y]); 
+	}
+			else {
+
+}
+			System.out.println(a[i] + "buzz");*/
+}
+	{
+	   
+	    	 System.out.println("Duplicate numbers");
+	    	 //Search for duplicate elements
+	    	 
+	    	int a[]  = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	 		int b []  = {11,33,33,65,88,66,44,99,11};
+			for (int i =0;i<a.length;i++) {
+	    		 for (int y = i + 1; y<b.length;y++) {
+	    			 if (a[i]==b[y])
+	    				 System.out.println(a[i]);
+	    					 
+	    		 }
+	    	 }
+	    		 
+
+	}
+	}
