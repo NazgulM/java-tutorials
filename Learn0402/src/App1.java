f8d43f59d8797be7190d24fa20bdd1d01e02a6c5
Cyngyz <92852651+NazgulM@users.noreply.github.com>
2022-02-05 18:25:06 +0100

    04022022

---
+++ Learn0402/src/App1.java
@@ -0,0 +1,29 @@
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sleeping z-z-z- ...");
+		
+		/*for (int i = 0; i<10; i=i+2) {
+			if(i==4) {
+				continue;
+			}
+			System.out.println(i);*/
+			
+			int a = 0;
+			while(a < 10) {
+				System.out.println(a);
+				a++;
+				if (a == 5) {
+					break;
+				}
+				
+			}
+			
+			}
+		}
+	
+
+
